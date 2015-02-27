635b6140fb93562ada59564eb0f97ed17afbe485
zhouyunke <zhouyunke@10.12.8.36>
2015-02-27 17:17:49 +0800

    s

--- src/SolarisDemo.java
+++ src/SolarisDemo.java
@@ -26,7 +26,7 @@ public class SolarisDemo extends JFrame {
 
 		try {
 			SolarisDemo frame = new SolarisDemo();
-			frame.setTitle("Solar");
+			frame.setTitle("Solariseklfehkjhef");
 			frame.setSize(500, 500);
 			frame.setVisible(true);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
